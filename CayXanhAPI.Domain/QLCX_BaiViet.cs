41bb02f51c8690e4137b09307f4d6f954d798a81
hoanghieu <hth0901@gmail.com>
2021-08-25 08:09:07 +0700

    ~~

---
+++ CayXanhAPI.Domain/QLCX_BaiViet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CayXanhAPI.Domain
+{
+    public class QLCX_BaiViet
+    {
+        public Guid ID { get; set; }
+        public int MaChuyenMuc { get; set; }
+        public string TieuDe { get; set; }
+        public string TomTat { get; set; }
+        public string NoiDung { get; set; }
+        public DateTime NgayDang { get; set; }
+        public string NguonTin { get; set; }
+        public string TuKhoa { get; set; }
+    }
+}
---
+++ CayXanhAPI.Domain/QLCX_CayCamTrongHanChe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CayXanhAPI.Domain
+{
+    public class QLCX_CayCamTrongHanChe
+    {
+        public Guid ID { get; set; }
+        public string TenCay { get; set; }
+        public string TenKhoaHoc { get; set; }
+        public string HoThucVatj { get; set; }
+        public string MoTa { get; set; }
+        public string CamTrong { get; set; }
+
+    }
+}
